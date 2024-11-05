6b9c6794eaeb071429405d38492a2748f0a26c7b
ntc1072002 <86583893+ntc1072002@users.noreply.github.com>
2024-11-05 10:37:04 +0700

    Version.js

--- Version.js
+++ Version.js
@@ -1,4 +1,5 @@
 {
     "latest_version": "1.2.3",
-    "update_url": "https://yourserver.com/path_to_your_app_installer.exe"
+    "update_url": "https://drive.google.com/drive/folders/1KjFqxLAQhaxPGDtnBzAAadLm5rJkVD2S?usp=sharing"
 }
+

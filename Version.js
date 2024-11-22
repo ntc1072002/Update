32915a47a915d70cad294f9b9005f34308070b64
ntc1072002 <86583893+ntc1072002@users.noreply.github.com>
2024-11-22 17:53:24 +0700

    Version.js

--- Version.js
+++ Version.js
@@ -1,5 +1,5 @@
 {
-    "latest_version": "2.0.0",
+    "latest_version": "1.0.0",
     "update_url": "https://drive.usercontent.google.com/download?id=1RLJud6vSvE5jOoZbsNtqjL3qNfV34BIY&export=download&authuser=0&confirm=t&uuid=a36f89b7-5870-40fd-9ade-7e08d346f8e6&at=AENtkXaPZFYY38AKXLZ3s15I-wDF:1730790666695"
 }
 

ce75fe44c0c4e4e63d65f24cdf149636238e69fd
ntc1072002 <86583893+ntc1072002@users.noreply.github.com>
2024-11-23 16:32:26 +0700

    Version.js

--- Version.js
+++ Version.js
@@ -1,5 +1,5 @@
 {
     "latest_version": "1.0.0",
-    "update_url": "https://drive.usercontent.google.com/download?id=1jZstZilhxW5nM8VnPjnnR6ognKSKdPRx&export=download&authuser=0&confirm=t&uuid=17ed50ff-006f-41de-a3e3-7913d0590609&at=AENtkXaSCacoyh4ciExncA7wz1Ol:1732353969146"
+    "update_url": "https://drive.usercontent.google.com/download?id=1OJM43OyAf45DoQS_oLRe64KlvDBE9VNI&export=download&authuser=0&confirm=t&uuid=924ef97e-b888-4224-a880-a73662dc9852&at=AENtkXb1gJLzuFBHGsXn_AiQ9rOF:1732354277236"
 }
 

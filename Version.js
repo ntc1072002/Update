7346ff2e89621af08863e69bade2b444f31b9dc3
ntc1072002 <86583893+ntc1072002@users.noreply.github.com>
2024-12-16 10:25:13 +0700

    Version.js

--- Version.js
+++ Version.js
@@ -1,6 +1,17 @@
 {
     "latest_version": "1.0.0",
     "update_url": "https://drive.usercontent.google.com/download?id=1OJM43OyAf45DoQS_oLRe64KlvDBE9VNI&export=download&authuser=0&confirm=t&uuid=82ea1e02-7ed2-4b88-9454-9e08cf467d33&at=AENtkXbnlsCzJegNW1u6QErHJIEd:1732436667851",
-    "Zalo_url": "https://res-download-pc-te-vnso-pt-51.zadn.vn/win/ZaloSetup-24.12.1.exe"
+    "Zalo_url": "https://res-download-pc-te-vnso-pt-51.zadn.vn/win/ZaloSetup-24.12.1.exe",
+    "Chrome_url": "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B36743B12-ECFB-D644-6435-9FF5D351B20C%7D%26lang%3Dvi%26browser%3D5%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe",
+    "Photoshop_url": "https://drive.usercontent.google.com/download?id=1N-8uLiVZeTqhsLnybWHGPjos5-lH8wHF&export=download&authuser=1&confirm=t&uuid=df89b372-4c27-4a4e-bd9a-995d69a184e7&at=APvzH3pvw5sV8JmFpgqz3GnTrtNa:1733983239622",
+    "Illutrator_url": "https://download1640.mediafire.com/rzf1jim2gkzgM9f6vCrAPJXtAXMVbhNiVwwljaKbZOtJz7xFkxVHY7Q0fYvq0OaAsmJBxPS4RVnw_5dFitcJ3gsCRwGLg2p9uH-dqoo5mwEHU2TdFAWQ86b28MfQxjVTaMCU1dMm8bUHkMRBP3gaqEZ-GUx_01vPsbYq9CR9ykMUgA/p6usaw4spmhrqi3/Adobe+Illutrator+2024.zip",
+    "Unikey_url": "https://www.unikey.org/assets/release/unikey46RC2-230919-win32.zip",
+    "WinRAR_url": "https://www.win-rar.com/fileadmin/winrar-versions/winrar-x64-701vn.exe",
+    "Ultraview_url": "https://dl2.ultraviewer.net/UltraViewer_setup_6.6_vi.exe",
+    "Office Professional Plus (2021)_url": "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=Professional2021Retail&platform=x64&language=en-us&version=O16GA",
+    "Visio Pro (2021)_url": "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=VisioPro2021Retail&platform=x64&language=en-us&version=O16GA",
+    "Crack Office_url": "https://drive.usercontent.google.com/download?id=11qlPXT1Tbwp5DGl8SwJloEK5j6Uztxl8&export=download&authuser=0&confirm=t&uuid=24f5b893-1aa4-403f-98ec-837795e1c6d2&at=APvzH3rrZn2l7Do6RcMzld08P0oQ:1733570843345",
+    "Zoom_url": "https://cdn.zoom.us/prod/6.2.11.50939/ZoomInstaller.exe",
+    "Zalo_url": "https://res-download-pc-te-vnso-pt-51.zadn.vn/win/ZaloSetup-24.12.1.exe",
 }
 

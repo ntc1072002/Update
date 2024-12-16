d0451a04e8b67a04806bd27ea37168d1601ac637
ntc1072002 <86583893+ntc1072002@users.noreply.github.com>
2024-12-16 10:16:53 +0700

    Version.js

--- Version.js
+++ Version.js
@@ -1,5 +1,6 @@
 {
     "latest_version": "1.0.0",
-    "update_url": "https://drive.usercontent.google.com/download?id=1OJM43OyAf45DoQS_oLRe64KlvDBE9VNI&export=download&authuser=0&confirm=t&uuid=82ea1e02-7ed2-4b88-9454-9e08cf467d33&at=AENtkXbnlsCzJegNW1u6QErHJIEd:1732436667851"
+    "update_url": "https://drive.usercontent.google.com/download?id=1OJM43OyAf45DoQS_oLRe64KlvDBE9VNI&export=download&authuser=0&confirm=t&uuid=82ea1e02-7ed2-4b88-9454-9e08cf467d33&at=AENtkXbnlsCzJegNW1u6QErHJIEd:1732436667851",
+    "Zalo_url": "https://res-download-pc-te-vnso-pt-51.zadn.vn/win/ZaloSetup-24.12.1.exe"
 }
 
